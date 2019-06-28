2c7ad08b8fffe4e7bbc5de2d2f1b368a2d30ba25
815980812 <49382995+815980812@users.noreply.github.com>
2019-06-28 09:49:06 +0800

    Update User.java

--- sp01-commons/src/main/java/com/tedu/sp01/pojo/User.java
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/User.java
@@ -3,7 +3,7 @@ package com.tedu.sp01.pojo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-//同步
+//同步131313113
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
