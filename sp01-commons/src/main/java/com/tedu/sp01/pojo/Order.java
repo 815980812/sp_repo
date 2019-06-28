bf930f4d3b37839acdfe0e007f1637ee75dc56c5
贺昌昌 <815980812@qq.com>
2019-06-28 09:34:59 +0800

    7887

--- sp01-commons/src/main/java/com/tedu/sp01/pojo/Order.java
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Order.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Order {
-	//注解2
+	//注解22
 	private String id;
 	private User user;
 	private List<Item> items;
